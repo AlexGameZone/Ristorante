c684436ea2849b97c901c7dfce246b49af69a7ed
AlexGameZone <83956442+AlexGameZone@users.noreply.github.com>
2021-06-26 22:28:59 +0200

    Add files via upload

---
+++ ciao.java
@@ -0,0 +1,11 @@
+public class yes {
+public static void main(String[] args) {
+System.out.println("THE WORLD!");
+System.out.println("DIO!");
+System.out.println(1+4);
+System.out.println(args[0]+args[1]);
+int x= 9;
+int y=10;
+System.out.println(x+y);
+}
+}
